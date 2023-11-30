0fbc6beb3b7a249701486883f16c7b9c3152c462
HunterGooD <vlad.gudov@inbox.ru>
2023-11-30 10:13:19 +0400

    box with dtmx

--- predict_yolov8.py
+++ predict_yolov8.py
@@ -1,9 +1,7 @@
 from ultralytics import YOLO
 from PIL import Image
 
-model = YOLO('runs/detect/train2/weights/best.onnx')
-
-# model.export(format="saved_model", keras=True)
+model = YOLO('runs/detect/train3/weights/best.onnx')
 
 res = model("<image>")
 
@@ -13,3 +11,4 @@ for r in res:
     im = Image.fromarray(im_array[..., ::-1])  
     im.save('results.jpg') # image with outline dtmx
 res[0].save_crop(save_dir="crop", file_name="") # only dtmx
+res[0].boxes.xywh.tolist()[0] # box with dtmx
